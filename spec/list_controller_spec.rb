d15eac0f298c5585029c3ac941012324e3d95403
Eloy Duran <eloy.de.enige@gmail.com>
2012-07-14 19:42:40 +0200

    It apears that a swipe gesture recognizer always needs a change in both drag path axes.
    In this case we change the Y slightly: 30 -> 35.

--- spec/list_controller_spec.rb
+++ spec/list_controller_spec.rb
@@ -2,7 +2,11 @@ describe "List Controller" do
   tests ListController
 
   it "new view should appear after a swipe" do
-    flick 'number 3', :from => :left, :to => :right, :duration => 1
+    cell = view('number 3')
+    from = cell.convertPoint(CGPointMake(10, 30), toView:nil)
+    to   = cell.convertPoint(CGPointMake(cell.bounds.size.width - 10, 35), toView:nil)
+
+    flick cell, :from => from, :to => to
     view('new view').should.not == nil
   end
 end
